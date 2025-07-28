6f7908338de770f4d4e9b4e062a9a69c1a2f3876
MatthieuRouls <rouls.matthieu@gmail.com>
2025-07-28 16:49:49 +0200

    JavaPatates

---
+++ src/training/afpa/cda24060/Main/Exo/PommesEtPaniers.java
@@ -0,0 +1,31 @@
+package training.afpa.cda24060.Main.Exo;
+
+import java.util.Scanner;
+
+public class PommesEtPaniers {
+    public static void pommesEtPaniers() {
+        Scanner scanner=new Scanner(System.in);
+
+        int pommesRestantes;
+        int panier = 3;
+        int allersRetours = 0;
+
+        System.out.println("Veuillez indiquer le nombre de pommes :");
+        pommesRestantes =scanner.nextInt();
+
+        while (pommesRestantes >= 0) {
+            if (pommesRestantes >= 3) {
+                allersRetours++;
+                pommesRestantes = pommesRestantes - panier;
+            }
+            else {
+                allersRetours++;
+                break;
+            }
+        }
+
+        System.out.println("Il faudra effectuer " + allersRetours + "  allers-retours");
+
+        scanner.close();
+    }
+}
---
+++ src/training/afpa/cda24060/Main/Main/main.java
@@ -0,0 +1,8 @@
+import training.afpa.cda24060.Main.Exo.PommesEtPaniers;
+
+public class main {
+    public static void main(String[] args) {
+        PommesEtPaniers exo = new PommesEtPaniers();
+        exo.pommesEtPaniers();
+    }20
+}
